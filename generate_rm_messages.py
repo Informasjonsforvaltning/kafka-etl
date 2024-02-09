5082627e8fd3d6af820e869aabf1a0441450c6f5
Nils Ove Tendenes <50194012+NilsOveTen@users.noreply.github.com>
2024-02-09 13:32:41 +0100

    feat: rm messages (#1)

---
+++ generate_rm_messages.py
@@ -0,0 +1,37 @@
+import json
+import os
+import time
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def millis_now():
+    return int(round(time.time() * 1000))
+
+
+def create_rm_message(fdk_id):
+    return "{" + f""" \\"type\\": \\"{os.environ.get("MESSAGE_TYPE")}\\", \\"fdkId\\": \\"{fdk_id}\\", \\"timestamp\\": {millis_now()}, \\"graph\\": \\"\\" """ + "}"
+
+
+message_list = list()
+with open('./kafka_export.json') as kafka_export_file:
+    kafka_objects = json.load(kafka_export_file)
+    kafka_ids = list()
+    for obj in kafka_objects:
+        kafka_ids.append(obj["id"])
+
+    with open('./harvester_export.json') as harvester_export_file:
+        harvester_meta = json.load(harvester_export_file)
+        harvester_ids = list()
+        for obj in harvester_meta:
+            harvester_ids.append(obj["fdkId"])
+
+        for kafka_id in kafka_ids:
+            if kafka_id not in harvester_ids:
+                message_list.append(create_rm_message(kafka_id))
+
+messages = "\n".join(message_list)
+with open("messages.txt", 'w', encoding="utf-8") as messages_file:
+    messages_file.write(messages)
